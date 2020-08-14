f66c81fe47fd0254174ac2a15d67c35cef4a406c
alachhman <33943685+alachhman@users.noreply.github.com>
2020-08-14 15:55:23 -0400

    Got USPS API working

--- src/Components/Tracking.js
+++ src/Components/Tracking.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
 
-class Tracker extends React.Component{
-    constructor(props){
+
+class Tracker extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             trackingId: "9400128206335287197597"
         }
     }
 
-    async readData(){
-        fetch("https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=(" +
-            "<TrackRequest USERID=\"959NA0006949\">" +
-            "<TrackID ID=" + this.state.trackingId + "}></TrackID>" +
-            "</TrackRequest>)"
-        ).then(response => {
-            console.log(response);
-        })
+    async readData() {
+        fetch(trackingAPICall(this.state.trackingId))
+            .then(response => response.text())
+            .then(text => console.log(text))
     }
-    async componentDidMount(){
+
+    async componentDidMount() {
         await this.readData();
     }
 
-    render(){
-        return(<div>Tracker</div>)
+    render() {
+        return (<div>Tracker</div>)
     }
 }
 
+function trackingAPICall(trackingNum){
+    return "https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=" +
+        "<TrackRequest USERID=\"959NA0006949\">" +
+        "<TrackID ID=\"" + trackingNum + "\"/>" +
+        "</TrackRequest>"
+}
 
 export default Tracker;
