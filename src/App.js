ea4a1a41f3ee3048b3c90fa502c59919d694a9b7
alachhman <33943685+alachhman@users.noreply.github.com>
2020-08-14 10:02:57 -0400

    Fixed the Tracking.js fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Calculator from './Components/Calculator';
-import Header from './Components/Header';
 import './App.css'
+import Tracker from "./Components/Tracking";
 class App extends React.Component {
     render() {
         return(
             <div className={"App"}>
-                
+
                 <div className={"Body"}>
                     <Calculator/>
+                    <Tracker/>
                 </div>
                 <div className={"Footer"}>
                     {/*Empty For Now*/}
--- src/Components/Tracking.js
+++ src/Components/Tracking.js
@@ -3,29 +3,27 @@ import React from 'react';
 
 class Tracker extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
             trackingId: "9400128206335287197597"
         }
     }
 
-    readData(){
-        fetch(`https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=${
-            <TrackRequest USERID="959NA0006949">
-                <TrackID ID={this.state.trackingId}></TrackID>
-            </TrackRequest>
-        }`)
-        .then(response => {
+    async readData(){
+        fetch("https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=(" +
+            "<TrackRequest USERID=\"959NA0006949\">" +
+            "<TrackID ID=" + this.state.trackingId + "}></TrackID>" +
+            "</TrackRequest>)"
+        ).then(response => {
             console.log(response);
         })
     }
-    componentDidMount(){
-        this.readData();
+    async componentDidMount(){
+        await this.readData();
     }
 
-
     render(){
-        return(<div></div>)
+        return(<div>Tracker</div>)
     }
 }
 
