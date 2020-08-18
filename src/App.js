be821ae01ac92f3c6d2708b9daa8cab538255122
mahi-g <gm1585@nyu.edu>
2020-08-17 23:46:08 -0400

    Display USPS tracking info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Calculator from './Components/Calculator';
 import './App.css'
-import Tracker from "./Components/Tracking";
 class App extends React.Component {
     render() {
         return(
@@ -9,7 +8,6 @@ class App extends React.Component {
 
                 <div className={"Body"}>
                     <Calculator/>
-                    <Tracker/>
                 </div>
                 <div className={"Footer"}>
                     {/*Empty For Now*/}
--- src/Components/Calculations.js
+++ src/Components/Calculations.js
@@ -4,11 +4,11 @@ import "../App.css"
 const Calculations = (props) => {
     return (
         <div className="Cardstyle">
-            <p>PayPal Fee: <h3>${props.data.paypalFee}</h3></p>
-            <p>Seller Fee: <h3>${props.data.sellerFee}</h3></p>
-            <p>Shipping: <h3>${props.data.shippingFee}</h3></p>
-            <p>Revenue: <h3>${props.data.balance}</h3></p>
-            <p>Your profit: <h3>${props.data.itemProfit}</h3></p>
+            <p>PayPal Fee: <b>${props.data.paypalFee}</b></p>
+            <p>Seller Fee: <b>${props.data.sellerFee}</b></p>
+            <p>Shipping: <b>${props.data.shippingFee}</b></p>
+            <p>Revenue: <b>${props.data.balance}</b></p>
+            <p>Your profit: <b>${props.data.itemProfit}</b></p>
         </div>
     );
 };
