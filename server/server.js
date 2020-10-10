98adc18f6e00d33126e0a45dbc55a16aff06001a
mahi-g <gm1585@nyu.edu>
2020-10-09 23:53:42 -0400

    Update server.js, add routes

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ const db = require('./db');
 //create an instance of express
 const app = express();
 
-//mongo variable // middleware, read body
-
 //define env variables/ dbd configurations
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}`);
 });
 
-//middleware, can define multiple
+//middlewares
+app.use(express.json());
+
 app.use((req,res,next)=>{
     // res.status(404).json({
     //     status: "fail",
@@ -21,6 +21,7 @@ app.use((req,res,next)=>{
     //drop a req/packet by not calling next
     next();
 });
+
 
 //READ all expenses
 app.get('/expenses', async (req, res) => {
@@ -36,7 +37,7 @@ app.get('/expenses', async (req, res) => {
 //READ expense by user id
 app.get('/expenses/:id', async (req,res) => {
     try {
-        const results = await db.query(`select * from expenses where username = '${req.params.id}'`);
+        const results = await db.query(`select * from expenses where username = $1`, [req.params.id]);
         console.log(results.rows);
         res.status(200).json({
             status: "success",
@@ -52,32 +53,73 @@ app.get('/expenses/:id', async (req,res) => {
 });
 
 //WRITE an expense
-app.post('/expenses/add', async (req,res) => {
+app.post('/expenses/:id', async (req,res) => {
     try {
-        //const {username, paid, sold, shipping, date, platform, other} =
-        //const results = await db.query(`insert into expenses (username, paid, sold, shipping, date, platform, other) values ()`);
-        console.log(results.rows);
+        const {paid, sold, shipping, date, platform, other} = req.body;
+        const results = await db.query(`insert into expenses (username, paid, sold, shipping, date, platform, other) values ($1, $2, $3, $4, $5, $6, $7)`, 
+        [req.params.id, paid, sold, shipping, date, platform, other]);
+        //console.log(paid);
+        //console.log(req.params.id);
         res.status(200).json({
             status: "success",
-            data: {
-                id: 2
-            }
         })
     }
     catch(err) {
         console.log(err);
     }
+});
+
+//WRITE an tracking number
+app.post("/tracking/:id", async (req,res) => {
+    try{
+        const result = await db.query(`INSERT INTO tracking (username, tracking_num) VALUES ($1, $2)`, [req.params.id, req.body.tracking_num]);
+        res.status(200).json({
+            status: "success",
+        });
+    } catch(err) {
+        console.error(err);
+    }
     
 });
 
+
 //DELETE an expense
-app.delete('/expenses/:id', (req,res) => {
-    console.log(req.params.id);
+app.delete('/expenses/:transaction_id', async (req,res) => {
+    console.log(req.params.transaction_id);
+    const result = await db.query(`delete from expenses where transaction_id = $1`, [req.params.transaction_id]);
     res.status(200).json({
         status: "success",
         data: {
-            id: req.params.id
+            id: req.params.transaction_id
         }
     })
 });
 
+//DELETE a tracking number
+app.delete('/tracking/:tracking_id', async (req,res) => {
+    console.log(req.params.tracking_id);
+    await db.query(`delete from tracking where tracking_id = $1`, [req.params.tracking_id]);
+    res.status(200).json({
+        status: "success",
+    })
+});
+
+app.get('/tracking/', async (req, res) =>{
+    console.log();
+    const results = await db.query('SELECT * FROM tracking');
+    res.status(200).json({
+        status: "success",
+        data: {
+            tracking: results.rows
+        }
+        
+    });
+});
+
+// app.update("/users/:id", async(req, res) =>{
+//     try{
+//         const result = db.query(``);
+//     } catch(err){
+
+//     }
+// });
