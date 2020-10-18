e621139e51fed68222bf4a7596491ce6303c01fd
mahi-g <gm1585@nyu.edu>
2020-10-18 00:00:27 -0400

    Add JWT token/ authentication

--- server/server.js
+++ server/server.js
@@ -1,26 +1,58 @@
 const express = require("express");
 const cors = require('cors');
 const db = require('./db');
-require('dotenv').config()
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 //create an instance of express
 const app = express();
 
 //define env variables/ dbd configurations
+const accessTokenSecret = process.env.accessTokenSecret;
 const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}`);
 });
 
+
 //middlewares
 //enable cors for all routes
 app.use(cors());
 app.use(express.json());
 
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await db.query("SELECT password FROM users WHERE username = $1 AND password=$2", [username,password]);
+    console.log(user.rows);
+    if(user.rows.length !== 0 || user.row !== undefined){
+        const accessToken = jwt.sign({username, password}, accessTokenSecret, { expiresIn: '1h' });
+        res.json({accessToken});
+    } else {
+        res.send("Username or password incorrect");
+    }
+});
+
 //READ all expenses
-app.get('/expenses', async (req, res) => {
-    const results = await db.query("SELECT * FROM expenses");
+app.get('/expenses', authenticateJWT, async (req, res) => {
+    const results = await db.query("SELECT * WHERE username = $1", [req.user]);
     res.status(200).json({
         status: "success",
         data: {
@@ -30,7 +62,7 @@ app.get('/expenses', async (req, res) => {
 });
 
 //READ expense by user id
-app.get('/expenses/:id', async (req,res) => {
+app.get('/expenses/:id', authenticateJWT, async (req,res) => {
     try {
         const results = await db.query(`SELECT * FROM expenses WHERE username = $1`, [req.params.id]);
         console.log(results.rows);
@@ -44,16 +76,15 @@ app.get('/expenses/:id', async (req,res) => {
     catch(err) {
         console.log(err);
     }
-    
 });
 
 //WRITE an expense
-app.post('/expenses/:id', async (req,res) => {
+app.post('/expenses', async (req,res) => {
     try {
         const {paid, sold, shipping, other, paypal_fee, seller_fee, item_profit, platform, date} = req.body;
         const results = await db.query(`INSERT INTO expenses (username, paid, sold, shipping, other, paypal_fee, seller_fee, item_profit, platform, date) 
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`, 
-            [req.params.id, paid, sold, shipping, other, paypal_fee, seller_fee, item_profit, platform, date]
+            [req.user, paid, sold, shipping, other, paypal_fee, seller_fee, item_profit, platform, date]
         );
         res.status(200).json({
             status: "success",
