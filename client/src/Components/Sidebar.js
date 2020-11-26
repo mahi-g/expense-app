c76abd9de6f68c80283fce39fce328eb5b43d71b
mahi-g <gm1585@nyu.edu>
2020-11-26 02:07:37 -0500

    Align sidebar text

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -9,17 +9,47 @@ import IsoIcon from '@material-ui/icons/Iso';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 
-
+const divLayout = {
+    display: 'flex',
+}
+const LinkText = {
+    marginLeft:'25%', 
+    marginTop:'2px'
+}
 const Sidebar = () => {
     return(
         <div className="Sidebar">
             <ul>
-                <li><Link to="/dashboard"> <InsertChartIcon/>Dashboard</Link></li>
-                <li><Link to="/sales"> <ListAltIcon/>Sales</Link></li>
-                <li><Link to="/"> <IsoIcon/>Fee Calculator</Link></li>
-                <li><Link to="/track"><LocalShippingIcon/>Track Packages</Link></li>
-                <hr />
-                <li><Link to="/track"> <AccountCircleIcon/> Profile</Link></li>
+                <li>
+                    <Link to="/dashboard" style={divLayout}>
+                        <div><InsertChartIcon style={{margin:'auto'}} /></div>
+                        <div style={LinkText} >Dashboard</div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/sales" style={divLayout}>
+                        <div><ListAltIcon style={{margin:'auto'}} /></div>
+                        <div style={LinkText} >Sales</div> 
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/" style={divLayout}> 
+                        <div><IsoIcon style={{margin:'auto'}} /></div>
+                        <div style={LinkText} >Calculator</div>                 
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/track" style={divLayout}>
+                        <div><LocalShippingIcon style={{margin:'auto'}} /></div>
+                        <div style={LinkText} >Track Packages</div>    
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/track" style={divLayout}>
+                        <div><AccountCircleIcon style={{margin:'auto'}} /></div>
+                        <div style={LinkText} >Profile</div> 
+                    </Link>
+                </li>
             </ul>
         </div>
     );
